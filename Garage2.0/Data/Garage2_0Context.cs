0f8b1956ce734e7cd59bc558e0bda7a6bafe7428
Mikael Perming <mperming@gmail.com>
2023-11-02 14:39:11 +0100

    Updaterad Databas

--- Garage2.0/Data/Garage2_0Context.cs
+++ Garage2.0/Data/Garage2_0Context.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Garage2._0.Models;
+using Humanizer.Localisation;
 
 namespace Garage2._0.Data
 {
@@ -14,6 +15,21 @@ namespace Garage2._0.Data
         {
         }
 
-        public DbSet<Garage2._0.Models.ParkeratFordon> ParkeratFordon { get; set; } = default!;
+        public DbSet<ParkeratFordon> ParkeratFordon => Set<ParkeratFordon>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ParkeratFordon>().HasData(
+                 new ParkeratFordon { Id = 1, FordonsTyp = "Bil", RegNr = "123pop", Farg = "Röd", Marke = "Toyota", Modell = "Prius", AntalHjul = 4, AnkomstTid = DateTime.Parse("2023-11-02T12:15") },
+                 new ParkeratFordon { Id = 2, FordonsTyp = "Båt", RegNr = "123båt", Farg = "Vit", Marke = "Storebror", Modell = "Japp", AntalHjul = 0, AnkomstTid = DateTime.Parse("2023-11-02T12:15") },
+                 new ParkeratFordon { Id = 3, FordonsTyp = "Buss", RegNr = "456pop", Farg = "Blå", Marke = "Volvo", Modell = "V70", AntalHjul = 6, AnkomstTid = DateTime.Parse("2023-11-02T12:45") },
+                 new ParkeratFordon { Id = 4, FordonsTyp = "Flygplan", RegNr = "783pop", Farg = "Vit", Marke = "Airbus", Modell = "XX90", AntalHjul = 8, AnkomstTid = DateTime.Parse("2023-11-02T12:25") },
+                 new ParkeratFordon { Id = 5, FordonsTyp = "Motorcykel", RegNr = "098pop", Farg = "Svart", Marke = "Mazda", Modell = "Vroom", AntalHjul = 2, AnkomstTid = DateTime.Parse("2023-11-02T12:55") }
+
+              ) ;
+        }
     }
 }
