83a9d2a58eac1232ff6c30195888392a90979b50
Mikael Perming <mperming@gmail.com>
2023-11-02 14:02:35 +0100

    modellen

---
+++ Garage2.0/Models/ParkeratFordon.cs
@@ -0,0 +1,17 @@
+﻿namespace Garage2._0.Models
+{
+    public class ParkeratFordon
+    {
+
+        public string FordonsTyp { get; set; }
+        public string RegNr { get; set; }
+        public string Farg {  get; set; }
+        public string Marke { get; set; }
+        public string Modell { get; set; }
+        public int AntalHjul {  get; set; }
+        public DateTime AnkomstTid {  get; set; }
+
+
+
+    }
+}
